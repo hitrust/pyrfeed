092462b5bed5544d08d696b27b4955749473bb24
gissehel <gissehel@4a6ddd4c-5527-0410-b1f1-df347b4b51d2>
2007-01-20 18:21:46 +0000

    * Better rendering when using IE component and unicode chars.

--- pyrfeed/src/pyrfeed/Gui/wx/Controls/HtmlWindow.py
+++ pyrfeed/src/pyrfeed/Gui/wx/Controls/HtmlWindow.py
@@ -1,5 +1,6 @@
 import wx
 import wx.html
+from cStringIO import StringIO
 
 HTML_SIMPLE = 0
 HTML_COMPLEX = 1
@@ -19,6 +20,6 @@ if wx.Platform == '__WXMSW__':
 
     class HtmlComplex(wx.lib.iewin.IEHtmlWindow) :
         def ChangePage(self,content) :
-            self.LoadString("<html><head><style type='text/css'><!-- body,th,td,p { font-family:'Verdana, Arial, Helvetica, sans-serif'; } --></style></head><body>"+content+"</body>")
+            self.LoadStream(StringIO("<html><head><meta HTTP-EQUIV='Content-Type' CONTENT='text/html;charset=utf-8' /><style type='text/css'><!-- body,th,td,p { font-family:'Verdana, Arial, Helvetica, sans-serif'; } --></style></head><body>"+unicode(content).encode('utf-8')+"</body>"))
 
     HtmlClasses.append({'class':HtmlComplex,'name':'IE Component'})
